e210ef8a38fed87bbd1ebc189d70d7de3b31812f
hwisley0619 <hwisley0619@gmail.com>
2025-04-18 13:56:22 +0900

    WIP: Alter script table
    divide title with eng_title, kor_title

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -56,7 +56,8 @@ CREATE TABLE source_type (
 -- Create table: script
 CREATE TABLE script (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    eng_title VARCHAR(255) NOT NULL,
+    kor_title VARCHAR(255) NOT NULL,
     video_url VARCHAR(2048),
     source_type BIGINT,
     FOREIGN KEY (source_type) REFERENCES source_type(id)
